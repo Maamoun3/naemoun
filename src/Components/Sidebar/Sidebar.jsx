77db80f002b3cb5f048516c32e6611543ea80e50
Naim Boutros <naimboutros77@gmail.com>
2024-02-29 00:53:07 +0200

    add sidebar and components

---
+++ src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {IoReorderThreeOutline} from "react-icons/io5"
+
+const Sidebar = () => {
+return (
+    <div>
+        <div>
+            <div>
+                <img className='w-40' src="https//i.imgur.com/zpqwKLQ.png" alt="" />
+            </div>
+            <div>
+
+            </div>
+        </div>
+        <div>
+            <IoReorderThreeOutline/>
+            <p></p>
+        </div>
+    </div>
+)
+}
+
+export default Sidebar
